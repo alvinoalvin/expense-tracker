17bafc96ccb9be38c52bd2e30bc56ad3352b3ebf
alvinoalvin <alvinoalvin@gmail.com>
2021-07-23 22:07:58 -0700

    updated db with insert

--- api/db/create.sql
+++ api/db/create.sql
@@ -6,6 +6,8 @@ CREATE TABLE expenses (
   name VARCHAR(255) NOT NULL,
   category TEXT,
   cost decimal, 
-  date_created DATE,
+  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   deleted Boolean Default FALSE
 );
+
+insert into expenses (name, category, cost) VALUES ('Test', 'sampple', 20.00);
